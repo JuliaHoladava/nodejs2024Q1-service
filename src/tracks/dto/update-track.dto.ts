44e8a377d2cb7f67654aa464edac616dfa81b3b0
Julia Holadava <juliaholadava@gmail.com>
2024-03-10 21:49:27 +0100

    feat: add validation decorators for UpdateTrackDto

--- src/tracks/dto/update-track.dto.ts
+++ src/tracks/dto/update-track.dto.ts
@@ -1,6 +1,15 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class UpdateTrackDto {
+  @IsString()
   name?: string;
+
+  @IsString()
   artistId?: string | null;
+
+  @IsString()
   albumId?: string | null;
+
+  @IsNumber()
   duration?: number;
 }
