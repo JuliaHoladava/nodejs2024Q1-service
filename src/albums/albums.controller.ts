3278d3f3f444e63fdf393b6a6d5c61c2e9fd34e9
Julia Holadava <juliaholadava@gmail.com>
2024-03-11 22:33:05 +0100

    refactor: split business logic from AlbumsController into AlbumsService

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -7,22 +7,20 @@ import {
   Param,
   Body,
   HttpStatus,
-  NotFoundException,
   ParseUUIDPipe,
   HttpCode,
 } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
-import { Album } from './entities/album.entity';
+import { AlbumsService } from './albums.service';
 
 @Controller('album')
-export class ArtistsController {
-  private albums: Album[] = [];
+export class AlbumsController {
+  constructor(private readonly albumsServise: AlbumsService) {}
 
   @Get()
   getAllAlbums() {
-    return this.albums;
+    return this.albumsServise.getAllAlbums();
   }
 
   @Get(':id')
@@ -33,22 +31,13 @@ export class ArtistsController {
     )
     id: string,
   ) {
-    const album = this.albums.find((album) => album.id === id);
-    if (!album) {
-      throw new NotFoundException(`Album with ID ${id} not found`);
-    }
-    return album;
+    return this.albumsServise.getAlbumById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   createAlbum(@Body() createAlbumDto: CreateAlbumDto) {
-    const newAlbum = {
-      ...createAlbumDto,
-      id: uuidv4(),
-    };
-    this.albums.push(newAlbum);
-    return newAlbum;
+    return this.albumsServise.createAlbum(createAlbumDto);
   }
 
   @Put(':id')
@@ -56,29 +45,12 @@ export class ArtistsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateAlbumDto: UpdateAlbumDto,
   ) {
-    const albumIndex = this.albums.findIndex((album) => album.id === id);
-
-    if (albumIndex === -1) {
-      throw new NotFoundException(`Album with ID ${id} not found`);
-    }
-
-    this.albums[albumIndex] = {
-      ...this.albums[albumIndex],
-      ...updateAlbumDto,
-    };
-
-    return this.albums[albumIndex];
+    return this.albumsServise.updateAlbum(id, updateAlbumDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  deleteArtist(@Param('id', ParseUUIDPipe) id: string) {
-    const index = this.albums.findIndex((album) => album.id === id);
-
-    if (index === -1) {
-      throw new NotFoundException(`Album with ID ${id} not found`);
-    }
-
-    this.albums.splice(index, 1);
+  deleteAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumsServise.deleteAlbum(id);
   }
 }
