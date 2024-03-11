71ec6592fe36b68bad94b4d90dc47871e2fe93dc
Julia Holadava <juliaholadava@gmail.com>
2024-03-11 22:46:03 +0100

    feat: configure app port via .env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
